121fdffb542a97adb09e59c6301f1d69738827cf
Benjamin Höglinger <nefarius@dhmx.at>
2018-01-05 15:38:04 +0100

    Added missing license snippets Added tracing code

--- Trace.h
+++ Trace.h
@@ -29,6 +29,7 @@ Environment:
         WPP_DEFINE_BIT(TRACE_DEVICE)                                   \
         WPP_DEFINE_BIT(TRACE_QUEUE)                                    \
         WPP_DEFINE_BIT(TRACE_SIDEBAND)                                 \
+        WPP_DEFINE_BIT(TRACE_GUARDIAN)                                 \
         )                             
 
 #define WPP_FLAG_LEVEL_LOGGER(flag, level)                                  \
