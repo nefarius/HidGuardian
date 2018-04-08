dafe1d391255f88ef8d871cbb911efa97d7b2491
Benjamin Höglinger <nefarius@dhmx.at>
2018-04-08 23:30:38 +0200

    Added logging

--- src/Vigils.NET/HidCerberus.Vigils.Core.Webhooks/Public/EntryPoint.cs
+++ src/Vigils.NET/HidCerberus.Vigils.Core.Webhooks/Public/EntryPoint.cs
@@ -1,7 +1,21 @@
-﻿namespace HidCerberus.Vigils.Core.Webhooks.Public
+﻿using System.IO;
+using System.Reflection;
+using Serilog;
+
+namespace HidCerberus.Vigils.Core.Webhooks.Public
 {
     public class EntryPoint
     {
+        static EntryPoint()
+        {
+            var assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.RollingFile(Path.Combine(assemblyFolder, "HidCerberus.Vigils.Core.Webhooks-{Date}.log"))
+                .CreateLogger();
+        }
+
         /// <summary>
         ///     Gets called by HidCerberus when an access decision has to be made.
         /// </summary>
