ced333fd254b62f5a50e1c8436c5e9be2fa2b703
Benjamin Höglinger <nefarius@dhmx.at>
2018-05-03 11:52:06 +0200

    Updated method prototype

--- src/Vigils.NET/HidCerberus.Vigils.Core.Webhooks/Public/EntryPoint.cs
+++ src/Vigils.NET/HidCerberus.Vigils.Core.Webhooks/Public/EntryPoint.cs
@@ -16,18 +16,16 @@ namespace HidCerberus.Vigils.Core.Webhooks.Public
                 .CreateLogger();
         }
 
-        /// <summary>
-        ///     Gets called by HidCerberus when an access decision has to be made.
-        /// </summary>
-        /// <param name="hardwareId">The Hardware ID identifying </param>
-        /// <param name="processId"></param>
-        /// <param name="isAllowed"></param>
-        /// <param name="isPermanent"></param>
-        /// <returns></returns>
-        public static bool ProcessAccessRequest(string hardwareId, uint processId, out bool isAllowed, out bool isPermanent)
+        public static bool ProcessAccessRequest(
+            string hardwareId,
+            string deviceId,
+            string instanceId,
+            uint processId,
+            out bool isAllowed,
+            out bool isPermanent
+        )
         {
-            isAllowed = false;
-            isPermanent = false;
+            isAllowed = isPermanent = false;
 
             return false;
         }
