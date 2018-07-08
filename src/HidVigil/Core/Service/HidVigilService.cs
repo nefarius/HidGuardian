fde0c6a4ea8322916aab0b217ead1b1de24e2465
Benjamin Höglinger <nefarius@dhmx.at>
2018-07-08 16:44:45 +0200

    Added webserver listener

--- src/HidVigil/Core/Service/HidVigilService.cs
+++ src/HidVigil/Core/Service/HidVigilService.cs
@@ -1,15 +1,26 @@
-﻿namespace HidVigil.Core.Service
+﻿using System;
+using Nancy.Hosting.Self;
+using Serilog;
+
+namespace HidVigil.Core.Service
 {
     public class HidVigilService
     {
+        private NancyHost _nancyHost;
+
         public void Start()
         {
+            Log.Information("Service starting");
 
+            _nancyHost = new NancyHost(new Uri("http://localhost:35122"));
+            _nancyHost.Start();
         }
 
         public void Stop()
         {
+            Log.Information("Service stopping");
 
+            _nancyHost.Stop();
         }
     }
 }
