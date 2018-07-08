c251d86b7e2d24bb064b08a97ca7a17b3f2de7d4
Benjamin Höglinger <nefarius@dhmx.at>
2018-07-08 16:04:29 +0200

    Added nancy bootstrapper

---
+++ src/HidVigil/Nancy/CustomJsonSerializer.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace HidVigil.Nancy
+{
+    [UsedImplicitly]
+    public sealed class CustomJsonSerializer : JsonSerializer
+    {
+        public CustomJsonSerializer()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Formatting = Formatting.Indented;
+        }
+    }
+}
