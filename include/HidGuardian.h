3f2cad00c22521a573d34d7fec718e74e15ae82d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-07-30 14:03:16 +0200

    Added IOCTL_HIDGUARDIAN_ARRIVAL_NOTIFICATION

--- include/HidGuardian.h
+++ include/HidGuardian.h
@@ -65,6 +65,11 @@ DEFINE_GUID(GUID_DEVINTERFACE_HIDGUARDIAN,
                                                                     METHOD_BUFFERED,    \
                                                                     FILE_ANY_ACCESS)
 
+#define IOCTL_HIDGUARDIAN_ARRIVAL_NOTIFICATION      CTL_CODE(FILE_DEVICE_HIDGUARDIAN,   \
+                                                                    IOCTL_INDEX + 0x04, \
+                                                                    METHOD_BUFFERED,    \
+                                                                    FILE_ANY_ACCESS)
+
 
 #include <pshpack1.h>
 
