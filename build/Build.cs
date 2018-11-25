2f0c8c089616197df025b773d158973e3ad0171f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-11-25 15:56:08 +0100

    Fixed build script even more

--- build/Build.cs
+++ build/Build.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
@@ -15,11 +13,12 @@ using RP = Nuke.Common.IO.PathConstruction.RelativePath;
 
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
     [Solution("HidGuardian.sln")] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [Parameter] readonly string Configuration;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
@@ -75,17 +74,5 @@ class Build : NukeBuild
             }.ForEach((pair, i) => File.Copy(Solution.Directory / pair.Key, ArtifactsDirectory / pair.Value));
 
             #endregion
-        });
-
-    private Target Pack => _ => _
-        .DependsOn(Compile)
-        .Executes(() =>
-        {
-            MSBuild(s => s
-                .SetTargetPath(Solution)
-                .SetTargets("Restore", "Pack")
-                .SetPackageOutputPath(ArtifactsDirectory)
-                .SetConfiguration(Configuration)
-                .EnableIncludeSymbols());
         });
 }
